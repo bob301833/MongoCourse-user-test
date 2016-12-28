e7d81745793de8db4e0173f95aea19b4bd6291e8
yuhsing <bob301833@gmail.com>
2016-12-28 10:49:58 +0800

    add validation test

---
+++ test/validation_test.js
@@ -0,0 +1,11 @@
+import assert from 'assert';
+import User from '../src/user';
+
+describe('Validating records', () => {
+  it('requires a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === 'Name is required.');
+  });
+});
