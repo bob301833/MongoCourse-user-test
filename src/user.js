2f722b8088e364b264bd72e6ca61961081a2b1ba
yuhsing <bob301833@gmail.com>
2016-12-28 15:39:23 +0800

    define virtual type

--- src/user.js
+++ src/user.js
@@ -12,10 +12,13 @@ const UserSchema = new Schema({
     },
     required: [true, 'Name is required.']
   },
-  postCount: Number,
   posts: [PostSchema]
+});
+
+UserSchema.virtual('postCount').get(function() {
+  return this.posts.length;
 });
 
 const User = mongoose.model('user', UserSchema);
 
-module.exports = User;
+export default User;
