44e1f3d5d894cc29a683a30bb4fdb64fa0948982
yuhsing <bob301833@gmail.com>
2016-12-29 12:04:05 +0800

    add association test

--- test/association_test.js
+++ test/association_test.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import assert from 'assert';
 import User from '../src/user';
 import Comment from '../src/comment';
 import BlogPost from '../src/blogPost';
@@ -19,10 +20,34 @@ describe('Association', () => {
       .then(() => done());
   });
 
-  it.only('saves relation between a user and a blogpost', (done) => {
+  it('saves relation between a user and a blogpost', (done) => {
     User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
       .then((user) => {
-        console.log(user);
+        assert(user.blogPosts[0].title === 'JS is Great');
+        done();
+      });
+  });
+
+  it('saves a full relation graph', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then((user) => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(user.blogPosts[0].comments[0].content === 'Congrats on great post');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+
         done();
       });
   });
