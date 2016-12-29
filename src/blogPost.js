76ab351ed985d84a00970eef6409764ecdde49b1
yuhsing <bob301833@gmail.com>
2016-12-29 11:12:48 +0800

    create association with ref & add association test

---
+++ src/blogPost.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const BlogPostSchema = new Schema({
+  title: String,
+  content: String,
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'comment'
+  }]
+});
+
+const BlogPost = mongoose.model('blogPost', BlogPostSchema);
+
+export default BlogPost;
